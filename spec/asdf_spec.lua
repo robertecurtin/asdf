28e668b58e966d1afae68fff28e1ad1840fb9894
Robert Curtin <robertecurtin@gmail.com>
2019-12-24 00:02:41 -0500

    Added multiple digits to numbers

--- spec/asdf_spec.lua
+++ spec/asdf_spec.lua
@@ -53,9 +53,22 @@ describe('asdf', function()
   --   the_value_of_this_variable_should_be('a', 'sf')
   -- end)
 
-  it('should return any number', function()
+  it('should return any single digit number', function()
     for _, var in pairs(chars) do
       for _, value in pairs(chars) do
+        when_this_variable_is_declared_as_a_number(var, value)
+        the_value_of_this_variable_should_be(var, value)
+      end
+    end
+  end)
+
+  it('should return numbers with multiple digits', function()
+    for _, var in pairs(chars) do
+      for _, value in pairs({
+        'as',
+        'asdf',
+        'fdsaasdf'
+      }) do
         when_this_variable_is_declared_as_a_number(var, value)
         the_value_of_this_variable_should_be(var, value)
       end
@@ -71,12 +84,30 @@ describe('asdf', function()
     the_value_of_this_variable_should_be('a', 'f')
   end)
 
-  it('should add numbers', function()
+  it('should add numbers with multiple digits', function()
+    given_this_variable_has_been_declared_as_a_number('a', 'a')
+    given_this_variable_has_been_declared_as_a_number('s', 'sdaf') -- 1203 / d99
+    given_this_variable_has_been_declared_as_a_number('d', 'fdsa') -- 3210 / d228
+
+    when_these_numbers_are_added('a', 's', 'd')
+    the_value_of_this_variable_should_be('a', 'ssasf') -- 11013 / d327
+  end)
+
+  it('should subtract numbers', function()
     given_this_variable_has_been_declared_as_a_number('a', 'a')
     given_this_variable_has_been_declared_as_a_number('s', 's')
     given_this_variable_has_been_declared_as_a_number('d', 'd')
 
     when_these_numbers_are_subtracted('a', 'd', 's')
     the_value_of_this_variable_should_be('a', 's')
+  end)
+
+  it('should subtract numbers with multiple digits', function()
+    given_this_variable_has_been_declared_as_a_number('a', 'a')
+    given_this_variable_has_been_declared_as_a_number('s', 'sadfd') -- 10232 / d302
+    given_this_variable_has_been_declared_as_a_number('d', 'dsfa') -- 2130 / d160
+
+    when_these_numbers_are_subtracted('a', 's', 'd')
+    the_value_of_this_variable_should_be('a', 'dsad') -- 2102
   end)
 end)
--- src/Asdf.lua
+++ src/Asdf.lua
@@ -1,28 +1,38 @@
 return function()
   local vars = {
-  }
+    }
 
   local function number_to_asdf(n)
-    return ({
-      [0] = 'a',
-      [1] = 's',
-      [2] = 'd',
-      [3] = 'f'
-    })[n]
+    local o = ''
+    while n > 0 do
+      o = o .. ({
+        [0] = 'a',
+        [1] = 's',
+        [2] = 'd',
+        [3] = 'f'
+      })[n % 4]
+      n = math.floor(n / 4)
+    end
+    return o:reverse()
   end
 
   local function asdf_to_number(n)
-    return ({
-      a = 0,
-      s = 1,
-      d = 2,
-      f = 3
-    })[n]
+    local o = 0
+    for v in n:gmatch('%a') do
+      o = o * 4
+      o = o + ({
+        a = 0,
+        s = 1,
+        d = 2,
+        f = 3
+      })[v]
+    end
+    return o
   end
 
   return function(_args)
     local args = {}
-    for v in _args:gmatch('%a') do
+    for v in _args:gmatch('%a+') do
       table.insert(args, v)
     end
 
